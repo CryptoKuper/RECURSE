fef5e7fb240e0dda36bc13bae4e98774af51fdd2
Rafael <r.tukanov1981@gmail.com>
2023-12-16 00:06:03 +0600

    Добавил решение

---
+++ Задача 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int M = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Натуральные числа в промежутке от {M} до {N}:");
+        PrintNaturalNumbers(M, N);
+    }
+}
