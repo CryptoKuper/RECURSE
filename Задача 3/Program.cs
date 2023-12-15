bcd661223d246a3f6d1705fd4c2470bb64146fc1
Rafael <r.tukanov1981@gmail.com>
2023-12-16 01:09:17 +0600

    Добавить коммит

---
+++ Задача 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.WriteLine(arr[index]);
+            PrintArrayReverse(arr, index - 1);
+        }
+    }
+
+    static void Main()
+    {
+        int[] myArray = { 1, 2, 5, 10, 34 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReverse(myArray, myArray.Length - 1);
+    }
+}
